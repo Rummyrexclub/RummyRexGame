b1bd09537b806522da36b70a89d6c1fb83682bc6
Rummyrexclub <pavankalyanponnada.in@gmail.com>
2026-02-25 11:10:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Rummy Server Running");
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(process.env.PORT || 3000, () => {
